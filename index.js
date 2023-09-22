75392f51f408a8cd28618f3f7a6e7cb5a10f11a4
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-09-22 23:53:59 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  // Create a new array by copying the original 'cats' array
+  const newCats = [...cats];
+  
+  // Add the new cat 'name' to the end of the new array
+  newCats.push(name);
+
+  return newCats;
+}
+
+function prependCat(name) {
+  const newCats = [...cats];
+  newCats.unshift(name);
+  return newCats;
+}
+
+function removeLastCat() {
+  const newCats = [...cats];
+  newCats.pop();
+  return newCats;
+}
+
+function removeFirstCat() {
+  const newCats = [...cats];
+  newCats.shift();
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
+
